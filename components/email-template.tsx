1f97fe38900e57bb4c35cd42c2d998d358faf0c2
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-05 18:25:20 +0530

    Adding contact form with intact messages with good template

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -1,31 +1,43 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface EmailTemplateProps {
-  imageURl: string,
+  imageURl: string;
 }
 
 interface ContactEmailTemplateProps {
-  name: string,
-  email: string,
-  message: string
+  name: string;
+  email: string;
+  message: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  imageURl
+  imageURl,
 }) => (
   <div>
     <h1>Here is your image</h1>
-    <img src={imageURl} alt="image"/>
+    <img src={imageURl} alt="image" />
   </div>
 );
 
-export const ContactEmailTemplate: React.FC<Readonly<ContactEmailTemplateProps>> = ({
-  name, email, message
-}) => (
-  <div>
-    <h1>You got a message from Mentify</h1>
-    <h2>Name :- {name}</h2>
-    <h2>Email :- {email}</h2>
-    <h2>Message :- {message}</h2>
+export const ContactEmailTemplate: React.FC<
+  Readonly<ContactEmailTemplateProps>
+> = ({ name, email, message }) => (
+  <div
+    style={{
+      border: "2px solid #ccc",
+      padding: "20px",
+      borderRadius: "10px",
+      maxWidth: "600px",
+      margin: "auto",
+      backgroundColor: "#f9f9f9",
+      fontFamily: "Arial, sans-serif",
+    }}
+  >
+    <h1 style={{ color: "#333", textAlign: "center" }}>
+      You got a message from Contact Form
+    </h1>
+    <h2 style={{ color: "#555" }}>Name: {name}</h2>
+    <h2 style={{ color: "#555" }}>Email: {email}</h2>
+    <h2 style={{ color: "#555" }}>Message: {message}</h2>
   </div>
 );
